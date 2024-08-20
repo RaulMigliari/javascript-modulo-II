a11d7d5bfb1d63abac5aa685da89fd5247293f25
Lucas Muterle <raulmigliari5@gmail.com>
2024-08-20 16:04:42 -0300

    Métodos
    Fundamentos de métodos: funções atreladas a objetos

---
+++ aula45-metodos/metodos.js
@@ -0,0 +1,10 @@
+let pessoa = {
+    nome: "Raul",
+    idade: "21",
+    dizerOla() {
+        console.log("Olá, mundo! Meu nome é " + this.nome)
+    }
+}
+
+console.log(pessoa)
+pessoa.dizerOla()
